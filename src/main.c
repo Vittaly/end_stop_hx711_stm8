b362fa41eecd5c8d9674cf6983ecb24245127944
vitaly.burkut <vitaly.burkut@gmail.com>
2021-08-22 12:16:28 +0500

    move

--- src/main.c
+++ src/main.c
@@ -1,104 +1,22 @@
-/* Includes ------------------------------------------------------------------*/
-#include "stm8s.h"
-#include "stm8s_it.h"    /* SDCC patch: required by SDCC for interrupts */
-#include "HX711/HX711.h"
-
-
-#define LED_GPIO_PORT  (GPIOB)
-#define LED_GPIO_PINS  (GPIO_PIN_5)	
+//#include <Arduino.h>
+#include<stm8s_gpio.h>
+#include"HX711.h"
 
 
 
-#define END_STOP_PORT(GPIOC) 
-#define END_STOP_PIN (GPIO_PIN_4)
+void setup (void) 
+{GPIO_WriteHigh(GPIOA, GPIO_PIN_4);
 
-#define TARE_PORT(GPIOC) 
-#define TARE_PIN (GPIO_PIN_5)
-
-
-
-
-
-//Filter parameters
-float FK = 0.9;
-int tolerance = 3000; //
-int values_count = 5; //HX711 speed is 80Hz or near this value. To get 10 values the sensor need around 120ms.
-
-
-
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-void Delay (uint16_t nCount);
-
-/* Private functions ---------------------------------------------------------*/
-/* Public functions ----------------------------------------------------------*/
-
-/**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
-  */
-void main(void)
-{
-
-  /* Initialize I/Os in Output Mode */
-  GPIO_Init(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS, GPIO_MODE_OUT_PP_LOW_FAST);
-  GPIO_Init(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS, GPIO_MODE_OUT_PP_LOW_FAST);
-  GPIO_Init(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS, GPIO_MODE_OUT_PP_LOW_FAST);
-  GPIO_Init(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS, GPIO_MODE_OUT_PP_LOW_FAST);
-
-
-  HX711_init(128);
-
-
-  while (1)
-  {
-    /* Toggles LEDs */
-    GPIO_WriteReverse(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS);
-    Delay(0xFFFF);
-  }
+HX711_init(128);
 
 }
 
-/**
-  * @brief Delay
-  * @param nCount
-  * @retval None
-  */
-void Delay(uint16_t nCount)
-{
-  /* Decrement nCount value */
-  while (nCount != 0)
-  {
-    nCount--;
-  }
+void loop (void) {
+  GPIO_WriteHigh(GPIOA, GPIO_PIN_4);
 }
 
-#ifdef USE_FULL_ASSERT
 
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *   where the assert_param error has occurred.
-  * @param file: pointer to the source file name
-  * @param line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
+void main (void){
+ setup();
+  loop();
 }
-#endif
-
-/**
-  * @}
-  */
-
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
